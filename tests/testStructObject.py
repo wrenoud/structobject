dd376258fc1efebc966a92734c36c2aed5060ba7
Weston Renoud <wrenoud@gmail.com>
2014-04-24 07:44:10 -0700

    Added tests for parameter ordering

--- tests/testStructObject.py
+++ tests/testStructObject.py
@@ -1,8 +1,16 @@
 import sys
 import unittest
+import struct
 
 sys.path += ['..']
 from structObject import *
+from structField import *
+
+class Point(structObject):
+    "Basic point class"
+    _field_order = ('x','y')
+    x = ctype_double()
+    y = ctype_double()
 
 class structObjectTests(unittest.TestCase):
     # if length is specified it should be an array or 's'
@@ -10,6 +18,38 @@ class structObjectTests(unittest.TestCase):
         # exception should be raised if '_order' not defined in subclass
         # exception should be raise if '_order' defined in subclass that isn't first decendant of structObject
         pass
+
+    def testInitSetByAttribute(self):
+        p = Point()
+        self.assertEqual(p.items(),[('x', 0.0), ('y', 0.0)])
+        p.x = 5000.0
+        p.y = 300.5
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testInitImplicitOrder(self):
+        p = Point(5000.0, 300.5)
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testInitExplicitNames(self):
+        p = Point(y=300.5, x=5000.0)
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testInitMixedOrdering(self):
+        p = Point(5000.0, y=300.5)
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testInitImplicitList(self):
+        p = Point((5000.0, 300.5))
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testInitExplicitDict(self):
+        p = Point({'x':5000.0, 'y':300.5})
+        self.assertEqual(p.items(),[('x', 5000.0), ('y', 300.5)])
+
+    def testBinaryDouble(self):
+        p = Point(5000.0, 300.5)
+        self.assertEqual(p.pack(), struct.pack('dd', 5000.0, 300.5))
+        
 # no exception should be raised if parent has '_order' defined
 
 # trying to define a class with multiple parents should raise exception
